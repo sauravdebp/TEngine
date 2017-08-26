cd12e7657757d8405fe98677ee99442d349cb982
Saurav Deb Purkayastha <Saurav Deb Purkayastha>
2017-08-26 23:15:27 +0530

    Message

--- Test2.js
+++ Test2.js
@@ -4,10 +4,10 @@ var data = {
     answer: 0
 };
 
-var dataModel = TEngine.createDataModel(data);
+var dataModel = TEngine.createBindingModel(data);
 
 dataModel.updateEvents.n1 = dataModel.updateEvents.n2 = function() {
     dataModel.answer(parseInt(data.n1) + parseInt(data.n2));
 };
 
-TEngine.bindDataModel(dataModel);
+TEngine.bindModel(dataModel);
